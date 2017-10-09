41f7d6432b4896e58bd4f862fca6931464fd7a7a
j.maly2 <j.maly2@IR1-PC-08.musterschule.schule.paedml>
2017-10-09 17:25:56 +0200

    2.Version

--- test/vektorrechnung/VektorTest.java
+++ test/vektorrechnung/VektorTest.java
@@ -99,6 +99,7 @@ public class VektorTest {
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+        System.out.println("hello world");
     }
 
     /**
